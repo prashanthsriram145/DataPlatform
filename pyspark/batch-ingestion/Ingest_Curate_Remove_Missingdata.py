c8a2053a041c2a96ce6ac60daf6337b288967241
Prashanth Sriram <prashanthsriram145@gmail.com>
2025-09-13 19:29:27 +0530

    Changes to filter rows with null value for mandatory fields

--- pyspark/batch-ingestion/Ingest_Curate_Remove_Missingdata.py
+++ pyspark/batch-ingestion/Ingest_Curate_Remove_Missingdata.py
@@ -12,20 +12,22 @@ input_path = "./data/input/ncr_ride_bookings.csv"
 
 # Read CSV file
 df = spark.read.csv(input_path, header=True, inferSchema=True)
-df.show(truncate=False)
+print(df.count())
+# df.show(truncate=False)
 # Mandatory fields
-# mandatory_fields = ["customer_id", "name", "email"]
+mandatory_fields = ["'Payment Method'"]
 
 # # Filter out rows where any mandatory field is null or empty
-# curated_df = df.filter(
-#     " AND ".join([f"{col_name} IS NOT NULL AND {col_name} != ''" for col_name in mandatory_fields])
-# )
+curated_df = df.filter(
+    " AND ".join([f"{col_name} IS NOT NULL AND {col_name} != ''" for col_name in mandatory_fields])
+)
 
 # Show curated data
-# curated_df.show(truncate=False)
+print(curated_df.count())
+curated_df.show(truncate=False)
 
 # Optionally write curated data back to ADLS Gen2 in parquet/csv format
-# output_path = "abfss://container@storageaccount.dfs.core.windows.net/output/curated_data"
-# curated_df.write.mode("overwrite").parquet(output_path)
+output_path = "./data/output/curated_data"
+curated_df.write.mode("overwrite").parquet(output_path)
 
 spark.stop()
