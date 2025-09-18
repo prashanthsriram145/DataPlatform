263db55c0878fa337456d3171dfe9a1236cccbcd
Prashanth Sriram <prashanthsriram145@gmail.com>
2025-09-18 15:30:36 +0530

    changes to add scripts to deduplicate and curate data

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 pip install apache-airflow
 pip install pyspark
 pip freeze > requirements.txt
+
+Starting Spark server on local machine:
+
+spark-class org.apache.spark.deploy.master.Master
+spark-class org.apache.spark.deploy.worker.Worker spark://192.168.29.63:7077
--- pyspark/batch-ingestion/Ingest_Curate_Remove_Missingdata.py
+++ pyspark/batch-ingestion/Ingest_Curate_Remove_Missingdata.py
@@ -1,5 +1,8 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
+import numpy as np
+from pyspark.sql.functions import col, lit, when
+from pyspark.ml.feature import Imputer
 
 # Initialize Spark session
 spark = SparkSession.builder \
@@ -8,23 +11,52 @@ spark = SparkSession.builder \
 
 # Path to CSV file
 
-input_path = "./data/input/ncr_ride_bookings.csv"
+input_path = "./data/output/deduplicated_data/"
 
 # Read CSV file
-df = spark.read.csv(input_path, header=True, inferSchema=True)
+df = spark.read.parquet(input_path)
+print("Count of records read from parquet after deduplication: ", df.count())
+
+# List of columns to check, or use df.columns for all columns
+columns = df.columns
+
+df = df.withColumn("Customer Rating", col("Customer Rating").cast("float"))
+
+# Replace "null" string with None (which becomes null in Spark DataFrame)
+for c in columns:
+    df = df.withColumn(c, when(col(c) == "null", None).otherwise(col(c)))
+
+df.printSchema()
 print(df.count())
+
 # df.show(truncate=False)
 # Mandatory fields
-mandatory_fields = ["'Payment Method'"]
+mandatory_fields = ["Payment Method","Customer Rating Imputed"]
+
+# Defaulting to Cash if Payment Method is missing
+df = df.na.fill("Cash", subset=["Payment Method"])
+
+# Impute missing values in 'Customer Rating' column with mean or median
+imputer = Imputer(
+    inputCols=["Customer Rating"], 
+    outputCols=["Customer Rating Imputed"]
+).setStrategy("mean")  # or "median"
+
+df = imputer.fit(df).transform(df)
+
+# check if any nulls remain in the imputed column
+df.select(columns).where(col("Customer Rating Imputed").isNull()).show()
+print("Showing all imputed values")
+df.show(truncate=False)
+
 
 # # Filter out rows where any mandatory field is null or empty
-curated_df = df.filter(
-    " AND ".join([f"{col_name} IS NOT NULL AND {col_name} != ''" for col_name in mandatory_fields])
-)
+curated_df = df.na.drop(subset=mandatory_fields)
 
 # Show curated data
 print(curated_df.count())
-curated_df.show(truncate=False)
+select_fields = mandatory_fields + ["Booking Value"]
+curated_df.select(select_fields).show(truncate=False)
 
 # Optionally write curated data back to ADLS Gen2 in parquet/csv format
 output_path = "./data/output/curated_data"
